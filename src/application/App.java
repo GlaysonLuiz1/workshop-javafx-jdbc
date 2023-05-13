5c39dec58012c92be2179cba98612eac503a4306
GlaysonLuiz1 <Glaysonluiz1@gmail.com>
2023-05-13 19:10:36 -0300

    Main view controller

--- src/application/App.java
+++ src/application/App.java
@@ -12,12 +12,11 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("gui/MainView.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
 
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
-
 
             Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
